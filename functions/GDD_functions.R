3d34b5e07cad22ee80b43590de6b854c5ff07bab
VanEend <145325576+VanEend@users.noreply.github.com>
2025-10-27 10:01:44 +0100

    Create LOESS function for optimal hyperparameter search

--- functions/GDD_functions.R
+++ functions/GDD_functions.R
@@ -14,3 +14,33 @@ gdd_model <- function(tmean, tmax, t_ref, gdd_crit){
   
   return(doy)
 }
+
+#-------------------------------------------------------------------------------
+
+#MAE LOESS function
+# run model and compare to true values
+# returns the RMSE
+mae_gdd <- function(par, data) {
+  
+  df_mod <- data$model
+  df_val <- data$validation
+  
+  # calculate phenology predictions
+  # and put in a data frame
+  predict <- df_mod |>
+    group_by(year) |>
+    summarise(
+      prediction = gdd_model(
+        tmean = tmean,
+        tmax = tmax,
+        t_ref = par[1],
+        gdd_crit = par[2]
+      )
+    )
+  
+  predict <- na.omit(predict)
+  mae <- mean(abs(predict$prediction - df_val$doy), na.rm = T)
+  
+  # return rmse value
+  return(mae)
+}
